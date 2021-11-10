b5d795befb3fe0c18453b0e6b11f680e1ee73480
Matthias Wessendorf <mwessend@redhat.com>
2021-11-10 17:15:29 +0100

    Let's see here too ....
    Signed-off-by: Matthias Wessendorf <mwessend@redhat.com>

--- openshift/e2e-common.sh
+++ openshift/e2e-common.sh
@@ -138,3 +138,9 @@ function run_e2e_tests(){
   go_test_e2e -timeout=30m ./test/e2e/ \
     -imagetemplate "${TEST_IMAGE_TEMPLATE}" || fail_test "E2E suite failed"
 }
+
+function run_conformance_tests(){
+
+  go_test_e2e -timeout=30m ./test/e2e/conformance \
+    -imagetemplate "${TEST_IMAGE_TEMPLATE}" || fail_test "E2E conformance suite failed"
+}
