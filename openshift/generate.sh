fd5dc5d0d9312cc3ffc04f791ef86e74fc98b1e0
Christoph Stäbler <cstabler@redhat.com>
2024-06-26 09:43:10 +0200

    Make sure to install latest hack/generate

--- openshift/generate.sh
+++ openshift/generate.sh
@@ -4,6 +4,8 @@ set -euo pipefail
 
 repo_root_dir=$(dirname "$(realpath "${BASH_SOURCE[0]}")")/..
 
+# make sure we install the latest version
+rm -f $(go env GOPATH)/bin/generate
 GO111MODULE=off go install github.com/openshift-knative/hack/cmd/generate
 
 $(go env GOPATH)/bin/generate \
