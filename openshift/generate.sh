677b493bb2b3f5c8e78cf287c33fee648081ea1a
Serverless QE Robot <ocf-qe@redhat.com>
2025-02-28 19:49:59 -0500

    :open_file_folder: Update openshift specific files.

---
+++ openshift/generate.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+repo_root_dir=$(dirname "$(realpath "${BASH_SOURCE[0]}")")/..
+
+GOFLAGS='' go run github.com/openshift-knative/hack/cmd/generate@latest \
+  --root-dir "${repo_root_dir}" \
+  --generators dockerfile \
+  --dockerfile-image-builder-fmt "registry.ci.openshift.org/openshift/release:rhel-8-release-golang-%s-openshift-4.17"
